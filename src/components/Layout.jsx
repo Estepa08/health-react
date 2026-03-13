baa1be5e46d32718922cc1941623dc2197585b7f
Evgeny_Stepanov <step.evgeny@gmail.com>
2026-03-13 14:58:14 +0300

    refactor: переписал Layout на CSS Grid

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,35 +1,16 @@
-import React from 'react'
 import './Layout.css'
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <header className="layout__header">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1 className="layout__title">Health Survey</h1>
-            </div>
-          </div>
-        </div>
+        <h1 className="layout__title">Опросник Бернса</h1>
       </header>
 
-      <main className="layout__main">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-auto">{children}</div>
-          </div>
-        </div>
-      </main>
+      <main className="layout__main">{children}</main>
 
       <footer className="layout__footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <p className="layout__copyright">© 2024 Health Survey</p>
-            </div>
-          </div>
-        </div>
+        <p className="layout__copyright">© 2024 Health Survey</p>
       </footer>
     </div>
   )
